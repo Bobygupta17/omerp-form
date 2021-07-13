3ecd93b658b94c3dccc04cd8653beb205b9f29b9
Bobygupta17 <developer5@omlogistics.co.in>
2021-07-13 10:14:49 +0530

    design

--- src/app/pages/container-master/container-master.component.ts
+++ src/app/pages/container-master/container-master.component.ts
@@ -1,13 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-
+import {
+  NgbCalendar,
+  NgbDate,
+  NgbDateStruct,
+  NgbInputDatepickerConfig
+} from '@ng-bootstrap/ng-bootstrap';
 @Component({
   selector: 'app-container-master',
   templateUrl: './container-master.component.html',
   styleUrls: ['./container-master.component.scss']
 })
 export class ContainerMasterComponent implements OnInit {
-
-  constructor() { }
+  model: NgbDateStruct | undefined;
+  
 
   ngOnInit(): void {
   }
